0b741ba6dc3460ebcfeb9da91c47d0dfa80d3d9b
alisondragon <40364154+alisondragon@users.noreply.github.com>
2018-06-20 11:56:04 -0400

    620
    Former-commit-id: a54dfec9c8d6ec9e43636590a7ea404dca8af3e8

---
+++ Assets/HUB_Manager.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HUB_Manager : MonoBehaviour {
+
+    int score;
+
+    public Text scoretext;
+
+	// Use this for initialization
+	void Start () {
+        score = 0;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
